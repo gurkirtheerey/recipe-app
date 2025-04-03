7b15b65aea8219aac4e2205a4e360a8ac670e965
sunshinemoua <sunshinemoua21@gmail.com>
2025-04-03 13:18:26 -0700

    Refactor getRecipes function to remove userId parameter and update RecipesPage to fetch all recipes
    - Modified the getRecipes function to retrieve all recipes without filtering by user ID.
    - Updated the RecipesPage component to call the revised getRecipes function and log the fetched recipes.

--- app/recipes/actions.ts
+++ app/recipes/actions.ts
@@ -1,9 +1,10 @@
 import { createClient } from '@/lib/supabase/server';
 import { Recipe } from '../../types/recipeTypes';
 
-export async function getRecipes(userId: string): Promise<Recipe[]> {
+export async function getRecipes(): Promise<Recipe[]> {
+  
   const supabase = await createClient();
-  const { data, error } = await supabase.from('recipes').select('*').eq('user_id', userId);
+  const { data, error } = await supabase.from('recipes').select('*');
 
   if (error) {
     throw new Error(error.message);
