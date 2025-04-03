d54ab5828599eda10040a3331c206e63bc0587ca
sunshinemoua <sunshinemoua21@gmail.com>
2025-04-03 16:20:51 -0700

    Refactor recipe-rows.ts for improved code consistency and readability
    - Standardized string quotes from double to single quotes for consistency across the file.
    - Reformatted the destructuring of user data for better readability.
    - Updated comments for clarity and maintained the TODO for future enhancements regarding recipe collections.

--- app/data/recipe-rows.ts
+++ app/data/recipe-rows.ts
@@ -1,33 +1,35 @@
-import { getMyRecipes } from "../recipes/actions";
-import { favoriteRecipes } from "./dummy-recipes";
-import { createClient } from "@/lib/supabase/server";
+import { getMyRecipes } from '../recipes/actions';
+import { favoriteRecipes } from './dummy-recipes';
+import { createClient } from '@/lib/supabase/server';
 
 export async function getRecipeRows() {
   // Get the user from the session
   const supabase = await createClient();
-  const { data: { user } } = await supabase.auth.getUser()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  if (!user) return; 
+  if (!user) return;
 
   // Get the user's recipes
   const myRecipes = await getMyRecipes(user.id);
 
   return [
     {
-      title: "My Recipes",
+      title: 'My Recipes',
       items: myRecipes,
-      type: "recipe" as const,
+      type: 'recipe' as const,
     },
     {
-      title: "Favorite Recipes",
+      title: 'Favorite Recipes',
       items: favoriteRecipes, // still getting from dummy data
-      type: "recipe" as const,
+      type: 'recipe' as const,
     },
-  // TODO: Add recipe collections
-  //   {
-  //     title: "Recipe Collections",
-  //     items: collections,
-  //     type: "collection" as const,
-  //   },
+    // TODO: Add recipe collections
+    //   {
+    //     title: "Recipe Collections",
+    //     items: collections,
+    //     type: "collection" as const,
+    //   },
   ];
 }
