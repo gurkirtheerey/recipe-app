ab3e3c4ace3d48cef97682bee6a68ac052246f3f
Gurkirt Heerey <gheerey12@gmail.com>
2025-04-15 15:09:28 -0700

    remove ingredient parser for UI

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -6,7 +6,6 @@ import BackButton from '@/components/BackButton';
 import { StarRating } from '@/components/StarRating';
 import FavoriteButton from '@/components/FavoriteButton';
 import { RecipeWithFavorites } from '@/types/recipeTypes';
-import { parseIngredient } from '@/app/utils/recipeUtils';
 
 type RecipeParams = Promise<{
   id: string;
@@ -26,7 +25,7 @@ export default async function RecipePage({ params }: { params: RecipeParams }) {
   // e.g., "1 hour 30 minutes"
   const formattedTime: string = `${hours} hour${hours !== 1 ? 's' : ''} ${minutes} minute${minutes !== 1 ? 's' : ''}`;
 
-  const ingredientsWithMeasurements = recipe.ingredients?.map((ingredient: string) => parseIngredient(ingredient));
+  console.log(recipe);
 
   return (
     <main className="min-h-screen bg-[#f8f8f8]">
@@ -94,16 +93,11 @@ export default async function RecipePage({ params }: { params: RecipeParams }) {
           <div className="mb-8">
             <h2 className="text-lg font-medium mb-4">Ingredients</h2>
             <ul className="space-y-4">
-              {ingredientsWithMeasurements?.map(
-                (ingredient: { measurements: string; cleaned: string }, index: number) => (
-                  <li key={index} className="flex items-center justify-between py-2 border-b border-gray-100">
-                    <div className="flex items-center gap-3">
-                      <span className="text-gray-800">{ingredient.cleaned}</span>
-                    </div>
-                    <span className="text-gray-500">{ingredient.measurements}</span>
-                  </li>
-                )
-              )}
+              {recipe.ingredients?.map((ingredient: string, index: number) => (
+                <li key={index} className="flex items-center text-gray-800 py-2 border-b border-gray-100">
+                  <span className="">{ingredient}</span>
+                </li>
+              ))}
             </ul>
           </div>
 
