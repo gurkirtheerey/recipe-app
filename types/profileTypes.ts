4eb230e5c41bb0f6143074636361cf2e28fd0fba
Gurkirt Heerey <gheerey12@gmail.com>
2025-04-15 21:02:47 -0700

    add profile type

---
+++ types/profileTypes.ts
@@ -0,0 +1,10 @@
+export type Profile = {
+  id: string;
+  email: string;
+  username: string;
+  first_name: string;
+  last_name: string;
+  profile_picture: string;
+  created_at: string;
+  updated_at: string;
+};
