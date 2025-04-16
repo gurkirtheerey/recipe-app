6b40787561302f4b1cc056a3455d330cc0f88077
Gurkirt Heerey <gheerey12@gmail.com>
2025-04-16 14:04:55 -0700

    update add recipe modal

--- app/collections/[id]/page.tsx
+++ app/collections/[id]/page.tsx
@@ -3,22 +3,18 @@ import { useMutation, useQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
 import Loading from './loading';
 import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-} from '@/components/ui/dialog';
 import { Collection, Recipe } from '@/types';
 import { useEffect, useState } from 'react';
 import { RecipeCard } from '@/components/Recipe/RecipeCard';
 import { toast } from 'sonner';
 import { useQueryClient } from '@tanstack/react-query';
 import { ContextMenu, ContextMenuItem, ContextMenuContent, ContextMenuTrigger } from '@/components/ui/context-menu';
+import Modal from '@/components/Modal';
+import { useIsMobile } from '@/hooks/use-mobile';
+import { BadgeCheck, Plus } from 'lucide-react';
 
 const CollectionIdPage = () => {
+  const isMobile = useIsMobile();
   const queryClient = useQueryClient();
   const [isOpen, setIsOpen] = useState(false);
   const [selectedRecipes, setSelectedRecipes] = useState<Recipe[]>([]);
@@ -89,6 +85,10 @@ const CollectionIdPage = () => {
 
   const { collection, recipes, myRecipes } = data;
 
+  if (!collection) {
+    return <div>No collection</div>;
+  }
+
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="flex justify-between items-start mb-4">
@@ -96,33 +96,36 @@ const CollectionIdPage = () => {
           <h1 className="text-2xl font-bold">{collection.name}</h1>
           <p className="text-gray-500 text-sm">{collection.description}</p>
         </div>
-        <Dialog open={isOpen} onOpenChange={setIsOpen}>
-          <DialogTrigger asChild>
-            <Button>Add Recipe</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Add Recipe</DialogTitle>
-            </DialogHeader>
-            <DialogDescription>Add a recipe to your collection.</DialogDescription>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {myRecipes.map((recipe) => (
-                <div
-                  onClick={() => handleClick(recipe)}
-                  key={recipe.id}
-                  className={`border border-gray-200 bg-gray-200 rounded-lg p-4 shadow-md cursor-pointer hover:bg-gray-50 ${
-                    selectedRecipes.find((r) => r.id === recipe.id) ? 'bg-gray-500' : ''
-                  }`}
-                >
-                  <h2>{recipe.title}</h2>
-                </div>
-              ))}
-            </div>
-            <Button onClick={() => handleAddRecipes.mutate()}>
+        <Button variant="outline" onClick={() => setIsOpen(true)}>
+          <Plus className="w-4 h-4 mr-2" />
+          Add Recipe
+        </Button>
+        <Modal open={isOpen} onOpenChange={setIsOpen} title="Add Recipe" description="Add a recipe to your collection.">
+          <div className="flex flex-col gap-4 max-h-[300px] overflow-y-auto py-4 mb-4">
+            {myRecipes.map((recipe) => (
+              <div
+                onClick={() => handleClick(recipe)}
+                key={recipe.id}
+                className={
+                  'flex items-center justify-between border border-gray-200 bg-gray-200 rounded w-full px-4 py-2 shadow-md cursor-pointer hover:bg-gray-50'
+                }
+              >
+                <h2>{recipe.title}</h2>
+                {selectedRecipes.find((r) => r.id === recipe.id) && <BadgeCheck className="w-4 h-4" />}
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col gap-2 justify-end mb-4">
+            <Button disabled={handleAddRecipes.isPending} onClick={() => handleAddRecipes.mutate()}>
               {handleAddRecipes.isPending ? 'Adding...' : 'Add'}
             </Button>
-          </DialogContent>
-        </Dialog>
+            {isMobile && (
+              <Button variant="outline" onClick={() => setIsOpen(false)}>
+                Cancel
+              </Button>
+            )}
+          </div>
+        </Modal>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {recipes.map((recipe) => (
